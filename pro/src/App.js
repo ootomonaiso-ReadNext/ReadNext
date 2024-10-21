1421a7b9efc910565389a2550f5790d1705bacf1
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-10-21 23:51:11 +0900

    Refactor App.js to add authentication context and routes for login and home pages

--- pro/src/App.js
+++ pro/src/App.js
@@ -1,18 +1,29 @@
+// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './PrivateRoute';
+import Login from './components/Login';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          element={
-            <PrivateRoute>
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* ログインページ */}
+          <Route path="/login" element={<Login />} />
+          {/* トップページ */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
