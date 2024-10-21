78c7f39e46c1638912a353ee57bad8e8a821866e
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-10-21 23:06:33 +0900

    Refactor package.json to remove unnecessary dependencies and add new ones

--- pro/src/App.js
+++ pro/src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import HomePage from './pages/HomePage';
-import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route
+          element={
+            <PrivateRoute>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
--- pro/src/context/AuthContext.js
+++ pro/src/context/AuthContext.js
@@ -1,15 +1,23 @@
-import React, { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const login = () => {
+  useEffect(() => {
+    // ログイン状態をlocalStorageから取得
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token); // トークンがあれば認証済み
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem('token', token);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
+    localStorage.removeItem('token');
     setIsAuthenticated(false);
   };
 
