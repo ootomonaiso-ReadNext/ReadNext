7a877f8f1dd5a62bb158ac614e0cec63490274ee
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-10-27 23:09:28 +0900

    Refactor .env and firebaseConfig files to remove sensitive information

--- pro/firebaseConfig.js
+++ pro/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAAq40rM6nz6AbXW5DifTf8Axm1Ss5xQFw",
@@ -9,19 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "107285687559",
     appId: "1:107285687559:web:0b15b691e101ff081c29a2",
     measurementId: "G-8VBYEWKXQ9"
-};
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-export const signInWithGoogle = async () => {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const token = await result.user.getIdToken();
-    return token;
-  } catch (error) {
-    console.error("Google login error:", error);
-    throw error;
-  }
-};
+export { auth, googleProvider };
