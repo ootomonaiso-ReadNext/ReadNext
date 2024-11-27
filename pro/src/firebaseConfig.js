b4ce3bb04a9fec5c2a188fec704126300702181b
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-11-27 19:08:06 +0900

    firebaseConfig.jsの設定を環境変数を使用するように更新

--- pro/src/firebaseConfig.js
+++ pro/src/firebaseConfig.js
@@ -2,24 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-//const firebaseConfig = {
-//  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-//};
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAAq40rM6nz6AbXW5DifTf8Axm1Ss5xQFw",
-  authDomain: "readnext-10c36.firebaseapp.com",
-  projectId: "readnext-10c36",
-  storageBucket: "readnext-10c36.appspot.com",
-  messagingSenderId: "107285687559",
-  appId: "1:107285687559:web:0b15b691e101ff081c29a2",
-  measurementId: "G-8VBYEWKXQ9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
