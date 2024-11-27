eef5218c8a4ac88933c5c5ef815a7d34266551af
ootomonaiso <ootomonaiso.tubusiki@gmail>
2024-11-27 11:33:23 +0900

    React Routerの使用方法を更新し、HashRouterを追加

--- pro/src/App.js
+++ pro/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Routerのインポートを削除
 import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
@@ -16,84 +16,82 @@ import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/user-settings"
-            element={
-              <ProtectedRoute>
-                <UserSetting />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/bookshelf"
-            element={
-              <ProtectedRoute>
-                <UserBookshelfPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/add-from-database"
-            element={
-              <ProtectedRoute>
-                <AddBookFromDatabasePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/book-search"
-            element={
-              <ProtectedRoute>
-                <BookSearchPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads"
-            element={
-              <ProtectedRoute>
-                <ThreadListPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/new-thread"
-            element={
-              <ProtectedRoute>
-                <NewThreadPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads/:threadId"
-            element={
-              <ProtectedRoute>
-                <ThreadPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads/:threadId/comments"
-            element={
-              <ProtectedRoute>
-                <ThreadPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/user-settings"
+          element={
+            <ProtectedRoute>
+              <UserSetting />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/bookshelf"
+          element={
+            <ProtectedRoute>
+              <UserBookshelfPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-from-database"
+          element={
+            <ProtectedRoute>
+              <AddBookFromDatabasePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/book-search"
+          element={
+            <ProtectedRoute>
+              <BookSearchPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads"
+          element={
+            <ProtectedRoute>
+              <ThreadListPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/new-thread"
+          element={
+            <ProtectedRoute>
+              <NewThreadPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads/:threadId"
+          element={
+            <ProtectedRoute>
+              <ThreadPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads/:threadId/comments"
+          element={
+            <ProtectedRoute>
+              <ThreadPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </AuthProvider>
   );
 }
