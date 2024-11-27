959786dde638443a6609549dfe2dae967934179c
ootomonaiso <ootomonaiso.tubusiki@gmail>
2024-11-27 11:17:18 +0900

    ReactDOMのcreateRootを使用してアプリケーションのレンダリング方法を更新

--- pro/src/index.js
+++ pro/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // createRootを使用
+root.render(
   <GoogleOAuthProvider clientId="107285687559-nqjudb9mfrsaua8gdcjbjpt69d4f457r.apps.googleusercontent.com">
     <App />
-  </GoogleOAuthProvider>,
-  document.getElementById('root')
+  </GoogleOAuthProvider>
 );
