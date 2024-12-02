017a8da433b950e19d45b0d255c992d8e633c376
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2024-12-02 15:31:58 +0900

    Routerコンポーネントを削除し、Routesコンポーネントの構造を整理。コードの可読性を向上させるために、インポート文を整理。

--- pro/src/App.js
+++ pro/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
@@ -8,107 +8,105 @@ import Register from "./pages/Register";
 import UserSetting from "./pages/UserSetting";
 import UserBookshelfPage from "./pages/UserBookshelfPage";
 import AddBookFromDatabasePage from "./pages/AddBookFromDatabasePage";
-import BookSearchPage from "./pages/BookSearchPage"; 
+import BookSearchPage from "./pages/BookSearchPage";
 import ThreadListPage from "./pages/ThreadListPage";
 import NewThreadPage from "./pages/NewThreadPage";
-import ThreadPage from "./pages/ThreadPage"; 
-import UserMake from "./pages/UserMake"; 
+import ThreadPage from "./pages/ThreadPage";
+import UserMake from "./pages/UserMake";
 import PasswordReset from "./pages/PasswordReset";
 import ResetPasswordConfirm from "./pages/ResetPasswordConfirm";
-import VerifyEmail from "./pages/VerifyEmail"; // 追加
+import VerifyEmail from "./pages/VerifyEmail";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          {/* 公開ルート（認証不要） */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/usermake" element={<UserMake />} />
-          <Route path="/password-reset" element={<PasswordReset />} />
-          <Route path="/reset-password" element={<ResetPasswordConfirm />} />
-          <Route path="/verify-email" element={<VerifyEmail />} /> {/* 追加 */}
+      <Routes>
+        {/* 公開ルート（認証不要） */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/usermake" element={<UserMake />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
+        <Route path="/reset-password" element={<ResetPasswordConfirm />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
 
-          {/* 保護されたルート（認証が必要） */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/user-settings"
-            element={
-              <ProtectedRoute>
-                <UserSetting />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/bookshelf"
-            element={
-              <ProtectedRoute>
-                <UserBookshelfPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/add-from-database"
-            element={
-              <ProtectedRoute>
-                <AddBookFromDatabasePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/book-search"
-            element={
-              <ProtectedRoute>
-                <BookSearchPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads"
-            element={
-              <ProtectedRoute>
-                <ThreadListPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/new-thread"
-            element={
-              <ProtectedRoute>
-                <NewThreadPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads/:threadId"
-            element={
-              <ProtectedRoute>
-                <ThreadPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/books/:bookId/threads/:threadId/comments"
-            element={
-              <ProtectedRoute>
-                <ThreadPage />
-              </ProtectedRoute>
-            }
-          />
+        {/* 保護されたルート（認証が必要） */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/user-settings"
+          element={
+            <ProtectedRoute>
+              <UserSetting />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/bookshelf"
+          element={
+            <ProtectedRoute>
+              <UserBookshelfPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-from-database"
+          element={
+            <ProtectedRoute>
+              <AddBookFromDatabasePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/book-search"
+          element={
+            <ProtectedRoute>
+              <BookSearchPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads"
+          element={
+            <ProtectedRoute>
+              <ThreadListPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/new-thread"
+          element={
+            <ProtectedRoute>
+              <NewThreadPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads/:threadId"
+          element={
+            <ProtectedRoute>
+              <ThreadPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:bookId/threads/:threadId/comments"
+          element={
+            <ProtectedRoute>
+              <ThreadPage />
+            </ProtectedRoute>
+          }
+        />
 
-          {/* 404ページ */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+        {/* 404ページ */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </AuthProvider>
   );
 }
